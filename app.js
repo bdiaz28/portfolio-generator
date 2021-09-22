5f995d5827251e9fc86671cc22adddb38dcf66aa
bdiaz28 <brittanymdiaz@yahoo.com>
2021-09-22 15:39:33 -0400

    answer

--- app.js
+++ app.js
@@ -1,19 +1,93 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// inquirer
+// .prompt([
+//   {
+//     type: 'input',
+//     name: "name",
+//     message: "What is your name?"
+//   }
+// ])
 
-const profileDataArgs = process.argv.slice(2);
+// .then(answers => console.log(answers));
 
-console.log(profileDataArgs);
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      input: "input",
+      name: "about",
+      message: "Provide some information about yourself."
+    }
+    
+  ])
+};
 
-const [name, github] = profileDataArgs;
+promptUser().then(answers => console.log(answers));
 
-console.log(name, github);
+const promptProject = () => {
+  console.log(` 
+  ==================
+  Add a New Project
+  =================
+  `);
+  return  inquirer.prompt([
+    {
+      name: "input",
+      type: "name",
+      message: "What is the name of your project?"
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
 
-const pageHTML = generatePage(name, github);
+promptUser()
+.then(answers => console.log(answers))
+.then(promptProject)
+.then(projectAnswers => console.log(projectAnswers));
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
