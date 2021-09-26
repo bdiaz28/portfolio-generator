7f7d0826112583c4dfde629e267e68e6097aba22
bdiaz28 <brittanymdiaz@yahoo.com>
2021-09-26 17:31:31 -0400

    sections

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ promptUser()
     fs.writeFile('./index.html', pageHTML, err => {
       if (err) throw new Error(err);
 
-     console.log('Page created! Check out index.html in this directory to see it!');
+      console.log('Page created! Check out index.html in this directory to see it!');
     });
   });
